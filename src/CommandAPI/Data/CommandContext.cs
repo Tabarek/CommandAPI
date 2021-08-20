0b05fb66b5011c34e3772a39ac9af795113f3069
tabarek <tabarekaljaaf@yahoo.com>
2021-08-20 10:15:12 +0300

    Add CommandContext

---
+++ src/CommandAPI/Data/CommandContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using CommandAPI.Data;
+
+namespace CommandAPI.Data
+{
+    ///[DbContext] data representation class between the model and database
+    public class CommandContext : DbContext{
+        public CommandContext(DbContextOptions<CommandContext> options) : base(options){
+
+        }
+
+        /// [DbSet] representation of table in the database
+        /// We are telling or [DbContext] we want to "model" our commands in the database(store them as a table) 
+        public DbSet<Command> CommandItems{get; set;}
+    }
+}
